9e8eb3e4ef2ac31029a612d98cfb417732b38855
afzal9632 <mdafzal9632@gmail.com>
2022-11-10 23:18:24 +0530

    added various exception classes

---
+++ COVID/src/main/java/com/masai/exception/VaccinationCentreException.java
@@ -0,0 +1,16 @@
+package com.masai.exception;
+
+public class VaccinationCentreException extends Exception{
+
+	public VaccinationCentreException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public VaccinationCentreException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+}
---
+++ COVID/src/main/java/com/masai/exception/VaccineInventoryException.java
@@ -0,0 +1,15 @@
+package com.masai.exception;
+
+public class VaccineInventoryException extends Exception {
+
+	public VaccineInventoryException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public VaccineInventoryException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+}
